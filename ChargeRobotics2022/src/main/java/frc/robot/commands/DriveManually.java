227de5bd0b457148e7d1087c459d29d4a1dc0202
sierraciabattoni <94648535+sierraciabattoni@users.noreply.github.com>
2022-01-21 16:30:19 -0500

    Completed DriveManually command

--- ChargeRobotics2022/src/main/java/frc/robot/commands/DriveManually.java
+++ ChargeRobotics2022/src/main/java/frc/robot/commands/DriveManually.java
@@ -4,25 +4,52 @@
 
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
 
 public class DriveManually extends CommandBase {
   /** Creates a new DriveManually. */
-  public DriveManually() {
+
+private final Drivetrain m_drivetrain;
+
+private final DoubleSupplier m_leftSpeed;
+private final DoubleSupplier m_rightSpeed;
+private final DoubleSupplier m_forwardSpeed;
+private final DoubleSupplier m_reverseSpeed;
+
+private double m_currentSpeed = 0;
+
+  public DriveManually(DoubleSupplier leftSpeed, DoubleSupplier rightSpeed, DoubleSupplier forwardSpeed, DoubleSupplier reverseSpeed, Drivetrain subsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_drivetrain = subsystem;
+    m_leftSpeed = leftSpeed;
+    m_rightSpeed = rightSpeed;
+    m_forwardSpeed = forwardSpeed;
+    m_reverseSpeed = reverseSpeed;
+
+    addRequirements(m_drivetrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_drivetrain.stopDrive();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_drivetrain.tankDrive(m_leftSpeed.getAsDouble(), m_rightSpeed.getAsDouble(), true);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_currentSpeed = 0;
+    m_drivetrain.stopDrive();
+  }
 
   // Returns true when the command should end.
   @Override

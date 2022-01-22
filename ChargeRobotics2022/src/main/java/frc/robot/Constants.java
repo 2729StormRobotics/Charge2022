ce15c2236aa75f63fa1c3b636749b584b5968ef5
Josh Wright <653522@lrstudents.org>
2022-01-22 12:43:09 -0500

    largely completed the hanger subsystem

--- ChargeRobotics2022/src/main/java/frc/robot/Constants.java
+++ ChargeRobotics2022/src/main/java/frc/robot/Constants.java
@@ -5,17 +5,38 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-public static final class HangerConstants{
-    // This is a temporary fill-in value
-    public static final int kHangerMotorPort = 1;
-}
+    public static final class HangerConstants {
+        // These are all temporary fill-in values
+
+        // Gearing for calculations, given in output turns per motor turn
+        private static final double kGearing = 0.0;
+        private static final double kPulleyDiameter = 0.0; // Inches
+        public static final double kPulleyCircumference = Math.PI * kPulleyDiameter;
+
+        public static final double kDistancePerRotation = kPulleyCircumference * kGearing;
+        public static final double kSpeedPerRotation = kDistancePerRotation / 60;
+
+        public static final int kHangerMotorPort = 0;
+        public static final boolean kMotorInverted = false;
+        public static final double kClimbUpSpeed = 0.0;
+        public static final double kClimbDownSpeed = 0.0;
+        public static final double kMaxHeight = 0.0;
+        public static final int kPawlPistonChannel = 0;
+        public static final boolean kPawlPistonEnabled = false;
+        public static final boolean kPawlPistonDisabled = true;
+
+    }
 
 }
--- ChargeRobotics2022/src/main/java/frc/robot/subsystems/Hanger.java
+++ ChargeRobotics2022/src/main/java/frc/robot/subsystems/Hanger.java
@@ -4,50 +4,173 @@
 
 package frc.robot.subsystems;
 
-import frc.robot.Constants.HangerConstants.*;
+import static frc.robot.Constants.HangerConstants.*;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.REVLibError;
+
+import edu.wpi.first.wpilibj.Solenoid;
+
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-
-
 
 public class Hanger extends SubsystemBase {
 
   private final CANSparkMax m_hangerMotor;
 
-// private final RelativeEncoder m_encoder;
+  private final RelativeEncoder m_encoder;
 
-  /** Creates a new Hanger. */
+  private final Solenoid m_pawlPiston;
+  private boolean m_retracted = true;
+
+  /**
+   * Creates a new Hanger.
+   * 
+   */
   public Hanger() {
-m_hangerMotor = new CANSparkMax(3, MotorType.kBrushless);
+    m_hangerMotor = new CANSparkMax(kHangerMotorPort, MotorType.kBrushless);
+    motorInit(m_hangerMotor, kMotorInverted);
 
+    m_encoder = m_hangerMotor.getEncoder();
 
-// m_encoder = new RelativeEncoder() {
+    m_pawlPiston = new Solenoid(kPawlPistonChannel);
+    m_pawlPiston.set(kPawlPistonDisabled);
+
+    shuffleboardInit();
+
+  }
+
   
-// };
 
+  /**
+   * Initializes the hanger motor
+   * 
+   * @param motor The motor to be initialized
+   */
+  private void motorInit(CANSparkMax motor, boolean invert) {
+    motor.restoreFactoryDefaults(); // Resets the motors to default settings
+    motor.setIdleMode(IdleMode.kBrake); // Sets the motor to brake mode when idle
+    motor.setInverted(invert); // Invert the motor if needed
+    encoderInit(motor.getEncoder());
+  }
 
+  /**
+   * Initializes the encoder
+   * 
+   * @param encoder The encoder being reset
+   */
+  private void encoderInit(RelativeEncoder encoder) {
+    encoder.setPositionConversionFactor(kDistancePerRotation);
+    encoder.setVelocityConversionFactor(kSpeedPerRotation);
+    encoderReset(encoder);
+  }
+
+  /**
+   * Resets the encoder position to 0
+   * 
+   * @param encoder The encoder being reset
+   */
+  private void encoderReset(RelativeEncoder encoder) {
+    encoder.setPosition(0.0);
+  }
+
+  /**
+   * Drives the hanger motor
+   * 
+   * @param speed The motor speed at which to run the hanger
+   */
+  public void climb(double speed) {
+    m_hangerMotor.set(speed);
+  }
+
+  /**
+   * Drives the hanger motor so that the elevator rises using a constant speed
+   */
+  public void climbUp() {
+    climb(kClimbUpSpeed);
+  }
+
+  /**
+   * Drives the hanger motor so that the elevator descends using a constant speed
+   */
+  public void climbDown() {
+    climb(kClimbDownSpeed);
+  }
+
+  /**
+   * Stops the hanger motor
+   */
+  public void stopClimb() {
+    climb(0.0);
+  }
+
+  /**
+   * Get the current height
+   * 
+   * @return The current height, in inches
+   */
+  private double getHeight() {
+    return m_encoder.getPosition();
+  }
+
+  /**
+   * Get the current speed
+   * 
+   * @return The current speed, in inches per second
+   */
+  private double getSpeed() {
+    return m_encoder.getVelocity();
+  }
+
+  /**
+   * Determine if the climber is above its max height
+   * 
+   * @return true if the current height is above its maximum
+   */
+  public boolean atMaxHeight() {
+    return getHeight() > kMaxHeight;
+  }
+
+  /**
+   * Determine if the climber is below its min height
+   * 
+   * @return true if the current height is below its minimum
+   */
+  public boolean atMinHeight() {
+    return getHeight() < 0;
+  }
+
+  /**
+   * Engages the pawl piston, which locks the elevator at its current extension
+   */
+  public void engagePawlPiston() {
+    m_pawlPiston.set(kPawlPistonEnabled);
+  }
+
+  /**
+   * Disengages the pawl piston, which locks the elevator at its current extension
+   */
+  public void disengagePawlPiston() {
+    m_pawlPiston.set(kPawlPistonDisabled);
   }
 
 /**
- * Initializes the hanger motor.
- * @param motor The motor to be initialized
+ * Adds data to shuffleboard
  */
-  private void motorInit(CANSparkMax motor){
-    motor.restoreFactoryDefaults();
-    motor.setIdleMode(IdleMode.kBrake);
+private void shuffleboardInit(){
 
-
-
-  }
+}
 
   @Override
   public void periodic() {
+    if (getHeight() > 1) {
+      m_retracted = false;
+    } else {
+      m_retracted = true;
+    }
     // This method will be called once per scheduler run
-  }
+  }  
 }

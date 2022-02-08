c3313c734e652f7121845efeee267016d0ffeb43
irinale11 <irinanhule@gmail.com>
2022-02-08 11:01:50 -0500

    Worked on VisionAlign Command

--- ChargeRobotics2022/src/main/java/frc/robot/commands/VisionAlign.java
+++ ChargeRobotics2022/src/main/java/frc/robot/commands/VisionAlign.java
@@ -4,38 +4,43 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
-
-import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Vision;
+import frc.robot.subsystems.Drivetrain;
 
 import static frc.robot.Constants.VisionConstants.*;
 
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class VisionAlign extends PIDCommand {
-  /** Creates a new VisionAlign. */
-  private final Drivetrain m_drivetrain;
   private final Vision m_vision;
+  private final Drivetrain m_drivetrain;
+
+  /** Creates a new VisionAlign. */
+  public VisionAlign(Vision vision, Drivetrain drivetrain) {
+    super(
+        // The controller that the command will use
+        new PIDController(kAutoAlignP, kAutoAlignI, kAutoAlignD),
+        // This should return the measurement
+        () -> vision.getXOffset(),
+        // This should return the setpoint (can also be a constant)
+        () -> 0,
+        // This uses the output
+        output -> {
+          // Use the output here
+        });
+
+    m_vision = vision;
+    m_drivetrain = drivetrain;
   
-  public VisionAlign(Drivetrain drivetrain, Vision vision) {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(m_drivetrain);
     addRequirements(m_vision);
+    addRequirements(m_drivetrain);
 
-    super(newPIDController(), 0, null);
+    // Configure additional PID options by calling `getController` here.
   }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override

9bca3a2c331eeb34808da1d6681fecbb5f9a20d9
irinale629823 <irinanhule@gmail.com>
2022-03-24 19:09:30 -0400

    add index out one ball command

--- ChargeRobotics2022/src/main/java/frc/robot/commands/IndexOutOneBall.java
+++ ChargeRobotics2022/src/main/java/frc/robot/commands/IndexOutOneBall.java
@@ -10,6 +10,9 @@ import frc.robot.subsystems.Index;
 public class IndexOutOneBall extends CommandBase {
   /** Creates a new IndexOutOneBall. */
   private final Index m_index;
+
+  // Keeps track of whether or not there has been an upper ball at some point.
+  private boolean m_hadUpperBall = false;
   public IndexOutOneBall(Index index) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_index = index;
@@ -18,12 +21,34 @@ public class IndexOutOneBall extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_hadUpperBall = m_index.hasUpperBall();  
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    if (m_hadUpperBall) {
+      // There was an upper ball at some point, so just push out until there is no longer an upper ball
+      if (m_index.hasUpperBall()) {
+        m_index.runLowerIndexMotor(-0.6);
+        m_index.runUpperIndexMotor(-0.3);
+      } else {
+        // This actually means the command is finished
+        m_index.stopIndexMotors();
+      }
+    } else {
+      // There hasn't yet been an upper ball, so push up until there is an upper ball
+      if (!m_index.hasUpperBall()) {
+        m_index.runLowerIndexMotor(-0.6);
+        m_index.runUpperIndexMotor(-0.3);
+      } else {
+        // This is the first time that Index#hasUpperBall is returning true
+        m_index.stopIndexMotors();
+        m_hadUpperBall = true;
+      }
+      
+    }
   }
 
   // Called once the command ends or is interrupted.
@@ -33,6 +58,6 @@ public class IndexOutOneBall extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_hadUpperBall && !m_index.hasUpperBall();
   }
 }
